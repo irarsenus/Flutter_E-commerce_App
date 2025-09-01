6c6410f015f9cb417ecc48a23136ac7ce629408c
irarsenus <tech.irarsenal2012@gmail.com>
2025-09-01 09:00:39 +0200

    updates codes

--- lib/features/authentication/screens/onbaording.dart
+++ lib/features/authentication/screens/onbaording.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_gura1/utility/constant/image_strings_size.dart';
+import 'package:test_gura1/utility/constant/text_strings.dart';
 import 'package:test_gura1/utility/helpers/helper_functions.dart';
     class OnBoardingScreen extends StatelessWidget{
       const OnBoardingScreen ({super.key});
@@ -14,7 +15,18 @@ import 'package:test_gura1/utility/helpers/helper_functions.dart';
                   Image(
                     width: THelperFunctions.screenWidth() * 0.8,
                     height: THelperFunctions.screenHeight() * 0.6,
-                    image: AssetImage(TImages.onBoardingImage1),
+                    image: const  AssetImage(TImages.onBoardingImage1),
+                  ),
+                  Text(
+                      TTexts.onBoardingSubTitle1,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                    textAlign: TextAlign.center,
+                  ),
+const SizedBox(height: TSizes.spaceBtwItems),
+                  Text(
+                    TTexts.onBoardingSubTitle1,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                    textAlign: TextAlign.center,
                   ),
                 ],
               )
