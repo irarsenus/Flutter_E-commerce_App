1b7c492a5a36cbea3217bccc2cb2cce2e83ef10a
irarsenus <tech.irarsenal2012@gmail.com>
2025-09-01 07:55:19 +0200

    add features to onboarding file

--- lib/features/authentication/screens/onbaording.dart
+++ lib/features/authentication/screens/onbaording.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:test_gura1/utility/constant/image_strings_size.dart';
+import 'package:test_gura1/utility/helpers/helper_functions.dart';
     class OnBoardingScreen extends StatelessWidget{
       const OnBoardingScreen ({super.key});
 
@@ -10,7 +11,11 @@ import 'package:flutter/material.dart';
             children: [
               Column(
                 children: [
-                  Image(image: AssetImage(TImages.))
+                  Image(
+                    width: THelperFunctions.screenWidth() * 0.8,
+                    height: THelperFunctions.screenHeight() * 0.6,
+                    image: AssetImage(TImages.onBoardingImage1),
+                  ),
                 ],
               )
             ],
--- lib/utility/constant/image_strings_size.dart
+++ lib/utility/constant/image_strings_size.dart
@@ -1,4 +1,5 @@
 class TImages{
+  TImages._();
   // app logos
 static const String darkAppLogo = "";
 static const String lightAppLogo = "";
@@ -8,7 +9,7 @@ static const String facebook = "";
 
 //-- onBoarding images
   static const String  onBoardingImage1 = "Assets/images/on_boarding_images/onboarding.gif";
-  static const String  onBoardingImage1 = ;
-  static const String  onBoardingImage1 = ;
+  static const String  onBoardingImage2 = "Assets/images/on_boarding_images/onboarding.gif";
+  static const String  onBoardingImage3 = "Assets/images/on_boarding_images/onboarding.gif";
 
 }
